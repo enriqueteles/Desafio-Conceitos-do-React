12fa0abf2347af48008c221faf536a0e0ea3e943
Enrique Teles <enriquegteles@gmail.com>
2022-01-17 21:24:00 -0300

    Completa objetivos

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,35 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) 
+      return;
+    
+    let newTask: Task = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    const newTasks: Task[] = [...tasks];
+    newTasks.push(newTask);
+    setTasks(newTasks);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const targetTaskIndex = tasks.findIndex((task) => task.id === id);
+    const newTasks: Task[] = [...tasks];
+    newTasks[targetTaskIndex].isComplete = !(newTasks[targetTaskIndex].isComplete);
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const targetTaskIndex = tasks.findIndex((task) => task.id === id);
+    const newTasks: Task[] = [...tasks];
+    newTasks.splice(targetTaskIndex, 1);
+    setTasks(newTasks);
   }
 
   return (
